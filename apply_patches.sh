3145ebcc73f6ac6e8d972979bb9601fe1a5fdfa5
Masalha, Nael <nael.masalha@intel.com>
2019-11-18 12:02:12 +0200

    d4xx: port driver from kernel-4.4 to kernel-4.9
     - all features are enabled, except Y8 and Y12I
    
    Change-Id: I51fc36d202517d43a47e7e5665f87b01e9221084
    Signed-off-by: Masalha, Nael <nael.masalha@intel.com>

---
+++ apply_patches.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+set -e
+set -x
+
+apply_external_patches() {
+	git -C $1 am ${PWD}/perc_hw_ds5u_android-jetson_tx2/$1/*
+}
+
+apply_external_patches kernel/nvidia
+apply_external_patches kernel/kernel-4.9
+apply_external_patches hardware/nvidia/platform/t18x/common
+apply_external_patches hardware/nvidia/platform/t18x/quill
+
