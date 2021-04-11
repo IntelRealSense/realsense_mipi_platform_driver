5b2d38287269da3cca58ce12f0925e2988df2073
Emil Jahshan <emil.jahshan@intel.com>
2021-04-11 17:00:59 +0300

    json format to binary
    - removed unnecessary files and folders
    - now script works with windows as well as Linux
    - currently working with python (not python3), will convert
      in the future
    
    Signed-off-by: Emil Jahshan <emil.jahshan@intel.com>

---
+++ utilities/Input/Preset.h
@@ -0,0 +1,26 @@
+typedef struct {
+	TPresetName name;
+
+    STScpParams scpParams;
+
+    uint32_t autoExposureSetPoint;  // ok
+
+    T_COLOR_CORRECTION_MATRIX_FLOAT pColorCorrection;   // ok
+
+    // default for controls
+    uint32_t laserState;
+    uint32_t laserPower;
+    uint16_t roiTop;
+    uint16_t roiBottom;
+    uint16_t roiLeft;
+    uint16_t roiRight;
+    uint32_t autoExposure;
+    uint32_t manualExposure;
+    uint32_t gain;
+
+    uint32_t autoExposureFaceAuthLoops;
+    uint32_t exposureListSize;
+    uint32_t exposureList[AE_FACE_NOF_PARAMS_MAX];
+
+}STPreset;
+
