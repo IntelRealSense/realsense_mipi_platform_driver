4033b274092f1f2456a92e38db7b70b797bd4874
Xin Zhang <xin.x.zhang@intel.com>
2022-03-23 08:16:52 +0800

    Update README for JetPack 4.6.1 information
    - Update README for JetPack 4.6.1 links and folders.
    - Correct folder name downloaded by SDK Manager.
    
    Signed-off-by: Xin Zhang <xin.x.zhang@intel.com>

--- apply_patches.sh
+++ apply_patches.sh
@@ -7,6 +7,12 @@ if [ $# != 1 ]; then
     exit 1
 fi
 
+# NVIDIA SDK Manager's JetPack 4.6.1 source_sync.sh doesn't set the right folder name, it mismatches with the direct tar
+# package source code. Correct the folder name.
+if [ -d ../hardware/nvidia/platform/t19x/galen-industrial-dts ]; then
+    mv ../hardware/nvidia/platform/t19x/galen-industrial-dts ../hardware/nvidia/platform/t19x/galen-industrial
+fi
+
 apply_external_patches() {
     if [ $1 = 'apply' ]; then
         git -C ../$2 am ${PWD}/$2/*
@@ -18,5 +24,4 @@ apply_external_patches() {
 apply_external_patches $1 kernel/nvidia
 apply_external_patches $1 kernel/kernel-4.9
 apply_external_patches $1 hardware/nvidia/platform/t19x/galen/kernel-dts
-apply_external_patches $1 hardware/nvidia/platform/t19x/galen-industrial-dts/kernel-dts
 
--- apply_patches_ext.sh
+++ apply_patches_ext.sh
@@ -2,6 +2,12 @@
 set -e
 set -x
 
+# NVIDIA SDK Manager's JetPack 4.6.1 source_sync.sh doesn't set the right folder name, it mismatches with the direct tar
+# package source code. Correct the folder name.
+if [ -d $1/hardware/nvidia/platform/t19x/galen-industrial-dts ]; then
+    mv $1/hardware/nvidia/platform/t19x/galen-industrial-dts $1/hardware/nvidia/platform/t19x/galen-industrial
+fi
+
 apply_external_patches() {
 cat ${PWD}/$2/* | patch -p1 --directory=${PWD}/$1/$2/
 }
@@ -9,4 +15,3 @@ cat ${PWD}/$2/* | patch -p1 --directory=${PWD}/$1/$2/
 apply_external_patches $1 kernel/nvidia
 apply_external_patches $1 kernel/kernel-4.9
 apply_external_patches $1 hardware/nvidia/platform/t19x/galen/kernel-dts
-apply_external_patches $1 hardware/nvidia/platform/t19x/galen-industrial-dts/kernel-dts
