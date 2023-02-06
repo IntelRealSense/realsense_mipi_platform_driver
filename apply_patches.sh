0fb5aadb95d19e6d4a206730d8c77cb99da510db
Dmitry Perchanov <dmitry.perchanov@intel.com>
2023-02-06 15:37:33 +0200

    Default to single camera DT move dtsi to new logical folder single - dev board one/dual - evb
    Signed-off-by: Dmitry Perchanov <dmitry.perchanov@intel.com>

--- apply_patches.sh
+++ apply_patches.sh
@@ -3,15 +3,21 @@
 set -e
 
 if [[ $# < 1 ]]; then
-    echo "apply_patches.sh [--one-cam] apply [JetPack_version]"
+    echo "apply_patches.sh [--one-cam | --dual-cam] apply [JetPack_version]"
     echo "apply_patches.sh reset [JetPack_version]"
     exit 1
 fi
 
-# Default to dual camera DT for JetPack 5.0.2
+# Default to single camera DT for JetPack 5.0.2
+# single - dev board
+# one/dual - evb
 JP5_D4XX_DTSI="tegra194-camera-d4xx.dtsi"
 if [[ "$1" == "--one-cam" ]]; then
     JP5_D4XX_DTSI="tegra194-camera-d4xx-single.dtsi"
+    shift
+fi
+if [[ "$1" == "--dual-cam" ]]; then
+    JP5_D4XX_DTSI="tegra194-camera-d4xx-dual.dtsi"
     shift
 fi
 
@@ -41,7 +47,7 @@ apply_external_patches $1 hardware/nvidia/platform/t19x/galen/kernel-dts
 
 if [ $1 = 'apply' ]; then
     cp $DEVDIR/d4xx.c $DEVDIR/sources_$JETPACK_VERSION/kernel/nvidia/drivers/media/i2c/
-    cp $DEVDIR/$JP5_D4XX_DTSI $DEVDIR/sources_$JETPACK_VERSION/hardware/nvidia/platform/t19x/galen/kernel-dts/common/tegra194-camera-d4xx.dtsi
+    cp $DEVDIR/hardware/realsense/$JP5_D4XX_DTSI $DEVDIR/sources_$JETPACK_VERSION/hardware/nvidia/platform/t19x/galen/kernel-dts/common/tegra194-camera-d4xx.dtsi
 elif [ $1 = 'reset' ]; then
     rm $DEVDIR/sources_$JETPACK_VERSION/kernel/nvidia/drivers/media/i2c/d4xx.c
 fi
--- apply_patches_ext.sh
+++ apply_patches_ext.sh
@@ -2,14 +2,20 @@
 set -e
 
 if [[ $# < 1 ]]; then
-    echo "apply_patches_ext.sh [--one-cam] source_dir [JetPack_version]"
+    echo "apply_patches_ext.sh [--one-cam | --dual-cam] source_dir [JetPack_version]"
     exit 1
 fi
 
-# Default to dual camera DT for JetPack 5.0.2
+# Default to single camera DT for JetPack 5.0.2
+# single - dev board
+# one/dual - evb
 JP5_D4XX_DTSI="tegra194-camera-d4xx.dtsi"
 if [[ "$1" == "--one-cam" ]]; then
     JP5_D4XX_DTSI="tegra194-camera-d4xx-single.dtsi"
+    shift
+fi
+if [[ "$1" == "--dual-cam" ]]; then
+    JP5_D4XX_DTSI="tegra194-camera-d4xx-dual.dtsi"
     shift
 fi
 
@@ -35,4 +41,4 @@ apply_external_patches $1 hardware/nvidia/platform/t19x/galen/kernel-dts
 
 # For a common driver for JP4 + JP5 we override the i2c driver and ignore the previous that was created from patches
 cp $DEVDIR/d4xx.c $DEVDIR/$1/kernel/nvidia/drivers/media/i2c/
-cp $DEVDIR/$JP5_D4XX_DTSI $DEVDIR/$1/hardware/nvidia/platform/t19x/galen/kernel-dts/common/tegra194-camera-d4xx.dtsi
+cp $DEVDIR/hardware/realsense/$JP5_D4XX_DTSI $DEVDIR/$1/hardware/nvidia/platform/t19x/galen/kernel-dts/common/tegra194-camera-d4xx.dtsi
