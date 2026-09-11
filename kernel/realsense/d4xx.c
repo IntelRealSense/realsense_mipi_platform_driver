d7a93b5c3ebf7c47d13e5c0df9860bbdffe5585e
zhou <shengyong.zhou@realsenseai.com>
2026-09-11 16:57:06 +0800

    [HKR][gmsl] chore: sync dev snapshot into integration branch
    Update dev_gmsl_merge to the origin/dev snapshot at
    d5b77a61931a915d254069aae43ca11435b27154 as one aggregate patch.
    
    This brings in the current variable-height format support, D58x profiles and
    controls, DFU and SerDes recovery updates, JetPack geometry carriers, and
    driver version 1.0.6.14 while preserving a tree-identical dev snapshot.
    
    Signed-off-by: zhou <shengyong.zhou@realsenseai.com>

--- kernel/realsense/d4xx.c
+++ kernel/realsense/d4xx.c
@@ -8579,4 +8579,4 @@ MODULE_AUTHOR("Guennadi Liakhovetski <guennadi.liakhovetski@intel.com>,\n\
 				Shikun Ding <shikun.ding@intel.com>,\n\
 				Dmitry Perchanov <dmitry.perchanov@intel.com>");
 MODULE_LICENSE("GPL v2");
-MODULE_VERSION("1.0.6.13");
+MODULE_VERSION("1.0.6.14");
